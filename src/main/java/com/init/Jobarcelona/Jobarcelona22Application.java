24ef8c68dc41b44e6a63f38bbe8668c6eb8cf78e
Isabel Domenech <77856098+Isabel160609@users.noreply.github.com>
2022-05-30 20:03:27 +0200

    Add files via upload
    started proejct

---
+++ src/main/java/com/init/Jobarcelona/Jobarcelona22Application.java
@@ -0,0 +1,13 @@
+package com.init.Jobarcelona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jobarcelona22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jobarcelona22Application.class, args);
+	}
+
+}
