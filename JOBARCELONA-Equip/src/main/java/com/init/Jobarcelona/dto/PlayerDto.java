020ca812154a94735b6081dda817aecee4ee7a74
Isabel Domenech <77856098+Isabel160609@users.noreply.github.com>
2022-05-30 20:30:35 +0200

    Add files via upload

---
+++ JOBARCELONA-Equip/src/main/java/com/init/Jobarcelona/dto/PlayerDto.java
@@ -0,0 +1,24 @@
+package com.init.Jobarcelona.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PlayerDto {
+	
+	private int reference;
+    private int age;
+    private String nation;
+    private String league;
+    private String team;
+    private int goalsSelection;
+    private int SelectionsNation;
+    private String position;
+    private int price;
+    
+}
---
+++ JOBARCELONA-Equip/src/main/java/com/init/Jobarcelona/repository/PlayerRepository.java
@@ -0,0 +1,7 @@
+package com.init.Jobarcelona.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlayerRepository extends JpaRepository<Player, Integer>{
+
+}
