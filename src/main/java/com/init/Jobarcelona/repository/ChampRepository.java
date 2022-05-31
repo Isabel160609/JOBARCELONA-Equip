e3512df57fd784798433eb77621053fb2dde574b
Vanet <vanraf2004@yahoo.es>
2022-05-31 11:01:13 +0200

    Añadidos campos a entity y Dto

--- src/main/java/com/init/Jobarcelona/repository/ChampRepository.java
+++ src/main/java/com/init/Jobarcelona/repository/ChampRepository.java
@@ -3,5 +3,5 @@ package com.init.Jobarcelona.repository;
 import com.init.Jobarcelona.entity.Champ;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChampRepository extends JpaRepository<Champ, Integer> {
+public interface ChampRepository extends JpaRepository<Champ, Long> {
 }
