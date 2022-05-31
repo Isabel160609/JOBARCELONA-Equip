df73199e9227e1e9cea499cb265f40ea591dd9ae
Vanet <vanraf2004@yahoo.es>
2022-05-31 16:14:47 +0200

    Config  Swagger y POM con variaciones

--- src/main/java/com/init/Jobarcelona/core/config/SwaggerConfig.java
+++ src/main/java/com/init/Jobarcelona/core/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiKey;
@@ -14,8 +15,12 @@ import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
+
 
     private static final String AUTHORIZATION_HEADER = "Authorization";
 
@@ -47,5 +52,6 @@ public class SwaggerConfig {
         authorizationScopes[0] = authorizationScope;
         return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
     }
+
 
 }
